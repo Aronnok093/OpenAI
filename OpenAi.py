199ba025a197a0e17cfeed2e3aa3d1706e76291c
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2023-01-30 14:30:18 +0600

    Add files via upload

---
+++ OpenAi.py
@@ -0,0 +1,32 @@
+import openai
+openai.api_key="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"
+while True:
+    print("Enter Your Command.Ctrl-D to execute.");
+    cmd = [];
+    prompt="";
+    while True:
+        try:
+            tmp=input()
+        except:
+            break;
+        cmd.append(tmp)
+
+    print("Thinking....")
+    ln = len(cmd)
+    
+    for i in range(ln):
+        prompt+=cmd[i]+" "
+        
+    response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompt,
+                temperature=1,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+                )
+    result=response.choices[0].text
+    print(result)
+    print("Enter To continue")
+    input();
